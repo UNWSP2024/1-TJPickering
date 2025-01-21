b33b48cafeba63f36e87082558e863d04377e4bd
TJPickering <tjpickering@students.unwsp.edu>
2025-01-20 19:48:25 -0600

    Create program_2_method_2.py

---
+++ program_2_method_2.py
@@ -0,0 +1,8 @@
+#Programmer: Timothy Pickering
+#Date written: 1/20/2025
+#Title: Proverbs 22:29 Print_ALT
+print('''“Do you see a man skillful in his work?
+He will stand before kings;
+he will not stand before obscure men.”
+         (Proverbs 22:29)''')
+
